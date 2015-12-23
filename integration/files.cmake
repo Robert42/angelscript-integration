14ceb7e2591ed93fa9a713fee0f8b4b33a4469fe
Robert Hildebrandt <robert-42@web.de>
2015-12-23 09:02:04 +0100

    integrated into the cmake project

---
+++ integration/files.cmake
@@ -0,0 +1,21 @@
+set(SOURCE_FILES
+  include/angelscript-integration/angelscript-integration.h
+  src/angelscript-integration/angelscript-integration.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-classes.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-bvec-rgba.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-bvec-stpq.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-bvec-xyzw.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-dvec-rgba.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-dvec-stpq.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-dvec-xyzw.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators.h
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-ivec-rgba.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-ivec-stpq.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-ivec-xyzw.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-uvec-rgba.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-uvec-stpq.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-uvec-xyzw.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-vec-rgba.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-vec-stpq.cpp
+  src/angelscript-integration/glm/engine-library-math-vector-swizzle-operators-vec-xyzw.cpp
+)
